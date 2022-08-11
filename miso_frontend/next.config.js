ef8395a1a42f0c6ecbfcd04f90fab7f33655c516
Kohei909Otsuka <dashengkangping@gmail.com>
2022-08-11 22:15:32 +0900

    tsのcheckが効くようにする

--- miso_frontend/next.config.js
+++ miso_frontend/next.config.js
@@ -1,7 +1,21 @@
+// https://dev.to/chromygabor/add-typescript-type-check-to-next-js-2nbb
+// nextjsはデフォルトだとbuild時にしかtypescriptの型checkをいれてくれないので
+// 下記を書くことで開発中にリアルタイムでチェックできるようにしている
+
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack(config, options) {
+    const { dev, isServer } = options;
+
+    // Do not run type checking twice:
+    if (dev && isServer) {
+      config.plugins.push(new ForkTsCheckerWebpackPlugin());
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig

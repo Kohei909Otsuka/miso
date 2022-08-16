e258289f87c93c1d12e3d31f6c60201f3c141606
Kohei909Otsuka <dashengkangping@gmail.com>
2022-08-17 00:01:20 +0900

    carでしか動かないが詳細ページにCardをリンクさせる

--- miso_frontend/components/CarCard.tsx
+++ miso_frontend/components/CarCard.tsx
@@ -5,6 +5,7 @@ import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import Typography from '@mui/material/Typography';
+import Link from 'next/link'
 
 type CarCardProps = {
   name: string
@@ -22,20 +23,24 @@ const CarCard: React.FC<CarCardProps> = (props) => {
   } = props
 
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        height="194"
-        image={imageUrl ? imageUrl : "https://tomo-iki.jp/wp-content/uploads/2019/06/noimage.png"}
-        alt="image"
-      >
-      </CardMedia>
-      <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          {name}
-        </Typography>
-      </CardContent>
-    </Card>
+    <Link href={`/cars/${slug}`} >
+      <a>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            component="img"
+            height="194"
+            image={imageUrl ? imageUrl : "https://tomo-iki.jp/wp-content/uploads/2019/06/noimage.png"}
+            alt="image"
+          >
+          </CardMedia>
+          <CardContent>
+            <Typography variant="body2" color="text.secondary">
+              {name}
+            </Typography>
+          </CardContent>
+        </Card>
+      </a>
+    </Link>
   )
 }
 
